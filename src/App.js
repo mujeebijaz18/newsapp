f2aaffb9dbc6504fa67816f8f035eba331b6a1af
Mujeeb Ijaz <mujeeb_ijaz@hotmail.com>
2021-09-26 16:10:09 +0500

    1st commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello there. This is new React project.
+      </div>
+    )
+  }
+}
+
+
